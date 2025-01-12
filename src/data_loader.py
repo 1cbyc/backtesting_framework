533335f21058c114a41131d43e4b52f4e05ebdf2
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-01-12 01:26:09 +0100

    backtesting this ict model at 2025-01-12 01:26:09

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,4 +2,12 @@ import pandas as pd
 import yfinance as yf
 
 class DataLoader:
-    def
+    def __init__(self, symbol, start_date, end_date, interval='1d'):
+        self.symbol = symbol
+        self.start_date = start_date
+        self.end_date = end_date
+        self.interval = interval
+
+    def fetch_data(self):
+        """trying to fetch historical price data using yfinance"""
+        data = yf.download(self.symbol, start=self.start_date, end=self.end)
