8f750d3e044e705285c0a102ff0c461f1d5ec159
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-01-12 02:24:22 +0100

    backtesting this ict model at 2025-01-12 02:24:21

--- run_backtest.py
+++ run_backtest.py
@@ -3,3 +3,6 @@ from src.strategy import OrderBlockStrategy
 from src.backtester import Backtester
 from src.performance import Performance
 
+
+data_loader = DataLoader(symbol='EURUSD=X', start_date='2022-01-01', end_date='2023-01-01')
+data = data_loader.fetch_data()
