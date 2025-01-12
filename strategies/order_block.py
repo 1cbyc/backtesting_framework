e182bafd8c97ea8a32413791ef35ef220799b1a6
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-01-12 03:37:31 +0100

    backtesting this ict model at 2025-01-12 03:37:31

--- strategies/order_block.py
+++ strategies/order_block.py
@@ -3,4 +3,4 @@ class OrderBlockStrategy:
         self.name = "Order Block Strategy"
 
     def generate_signal(self, data):
-        
+        # putting a simple strategy based on order blocks (breakout above a block)
