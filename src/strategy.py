b610747da62c2de80c179c87af4122f54d703f46
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-01-12 02:40:47 +0100

    backtesting this ict model at 2025-01-12 02:40:47

---
+++ src/strategy.py
@@ -0,0 +1,28 @@
+class OrderBlockStrategy(ICTStrategy):
+    """basic order block strategy."""
+
+    def enter_trade(self, market_data):
+        """my logic to enter a trade based on order block."""
+        last_candle = market_data.iloc[-1]  # Get the last row (candle)
+        close = last_candle['Close']
+        high = last_candle['High']
+        low = last_candle['Low']
+
+        if close > high - 0.001:  # base sample logic
+            return {'type': 'buy', 'price': close}
+        elif close < low + 0.001:
+            return {'type': 'sell', 'price': close}
+        return None
+
+    def exit_trade(self, market_data):
+        """my logic to exit the trade."""
+        last_candle = market_data.iloc[-1]  # to get the last row (candle)
+        close = last_candle['Close']
+        high = last_candle['High']
+        low = last_candle['Low']
+
+        if close > high - 0.001:
+            return {'type': 'sell', 'price': close}
+        elif close < low + 0.001:
+            return {'type': 'buy', 'price': close}
+        return None
