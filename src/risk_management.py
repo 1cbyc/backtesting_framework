783e483d3da097530a8148800d89bf723ffca00a
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-01-12 01:50:25 +0100

    backtesting this ict model at 2025-01-12 01:50:25

--- src/risk_management.py
+++ src/risk_management.py
@@ -6,4 +6,7 @@ class RiskManagement:
     def calculate_position_size(self, stop_loss_distance):
         """want to calculate the position size based on the stop loss distance
         Args:
+            stop_loss_distance (float): The distance from the entry to the stop-loss price.
+        Returns:
+            float: The position size.
             """
