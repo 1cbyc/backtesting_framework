d6f22e0e4b98b6781db279897db55c745d6758d8
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-01-12 03:38:22 +0100

    backtesting this ict model at 2025-01-12 03:38:22

--- strategies/order_block.py
+++ strategies/order_block.py
@@ -6,4 +6,6 @@ class OrderBlockStrategy:
         # putting a simple strategy based on order blocks (breakout above a block)
         if data['close'].iloc[-1] > data['close'].rolling(window=5).mean().iloc[-1]:
             return 'buy'
-        
+        elif data['close'].iloc[-1] < data['close'].rolling(window=5).mean().iloc[-1]:
+            return 'sell'
+        return 'hold'
